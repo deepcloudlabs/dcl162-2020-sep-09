10d9840a94071f191e3c6d89365eea738847ecaf
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-10 15:57:12 +0300

    add delete rest api end point

--- hr-backend/hr-api.py
+++ hr-backend/hr-api.py
@@ -33,6 +33,9 @@ def getEmployees():
 set c:\DEVEL\stage\opt\curl-7.45.0\bin;%PATH%
 curl -X POST http://localhost:4400/hr/api/v1/employees -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"identity\": \"1\", \"fullName\": \"jack bauer\", \"iban\": \"tr1\", \"photo\" : null, \"birthYear\": 1956, \"salary\": 100000, \"department\": \"IT\", \"fulltime\": true}"
 curl -X POST http://localhost:4400/hr/api/v1/employees -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"identity\": \"2\", \"fullName\": \"kate austen\", \"iban\": \"tr2\", \"photo\" : null, \"birthYear\": 1986, \"salary\": 200000, \"department\": \"SALES\", \"fulltime\": true}"
+curl -X PUT http://localhost:4400/hr/api/v1/employees/2 -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"identity\": \"2\", \"fullName\": \"kate austen\", \"iban\": \"tr2\", \"photo\" : null, \"birthYear\": 1986, \"salary\": 200000, \"department\": \"SALES\", \"fulltime\": true}"
+curl -X PATCH http://localhost:4400/hr/api/v1/employees/2 -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"fulltime\": true}"
+curl -X DELETE http://localhost:4400/hr/api/v1/employees/2 -H "Accept: application/json"
 """
 
 
@@ -51,6 +54,13 @@ def updateEmployee(identity):
         {"$set": emp},
         upsert=False
     )
+    return jsonify(employee)
+
+
+@app.route("/hr/api/v1/employees/<identity>", methods=["DELETE"])
+def removeEmployee(identity):
+    employee = employees.find_one({"_id": identity})
+    employees.delete_one({"_id": identity})
     return jsonify(employee)
 
 
